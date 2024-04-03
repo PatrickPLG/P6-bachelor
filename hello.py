004d4ae1185f553a4591db8eddcdf2d5b400d772
PeterEllefsen <pellef@live.dk>
2024-04-03 14:39:42 +0200

    Added functionality for fetching images from DB and displaying them

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from bson.json_util import dumps
 from mongo_connect import client, upload_images_from_folder
 import gridfs
+from io import BytesIO
 
 app = Flask(__name__)
 print("test")
@@ -19,12 +20,29 @@ def get_data():
     # Convert the document to a JSON string and return it
     return dumps(document['plot'])
 
+@app.route('/get_images', methods=['GET'])
+def get_images():
+    db = client['artworks']
+    collection = db['images.files']
+    documents = collection.find({})
+    return dumps(documents)
+
+@app.route('/get_image/<image_id>', methods=['GET'])
+def get_image(image_id):
+    db = client['artworks']
+    fs_bucket = gridfs.GridFSBucket(db, bucket_name="images")
+
+    grid_out = fs_bucket.open_download_stream(image_id)
+    contents = grid_out.read()
+
+    return send_file(BytesIO(contents), mimetype='image/jpeg')
+
 @app.route('/upload_images', methods=['POST'])
 def upload_images():
     db = client['artworks']
-    fs = gridfs.GridFS(db, collection="images")  # Specify the "images" collection
+    fs = gridfs.GridFS(db, collection="images")  # billederne skal uploades til "images" collection
 
-    folder_path = r"C:\Users\pelle\Downloads\Artworks Highres\Artworks Highres"  # Use a raw string for the folder path
+    folder_path = r"Indsæt path her"  # r laver det til en raw string, så du kan skrive backslashes
     upload_images_from_folder(fs, folder_path)
     return jsonify({'message': 'Images uploaded successfully'}), 200
 
