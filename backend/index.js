64aa4bb689e72a4814aaef77e78d7a337e32e5a9
Daniel <daniel@canvy.com>
2024-04-04 16:50:16 +0200

    chore

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,7 @@ const { Server } = require("socket.io");
 const io = new Server(3000, { /* options */ });
 
 io.on("connection", (socket) => {
-    // ...
     console.log('Client connected');
-
-
     socket.on('message', (msg,callback) => {
         console.log(`Data from ${socket.id}:`, msg);
         callback();
@@ -16,4 +13,7 @@ io.on("connection", (socket) => {
 io.on("disconnect", (socket) => {
     console.log('Client disconnected: ', socket.id);
 })
+
+
+
 
