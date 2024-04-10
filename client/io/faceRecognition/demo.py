3fb3df4090cb0036f6516361c140cec22fe4918d
Daniel <daniel@canvy.com>
2024-04-10 14:28:38 +0200

    update

--- client/io/faceRecognition/demo.py
+++ client/io/faceRecognition/demo.py
@@ -14,15 +14,14 @@ import time
 from yunet import YuNet
 import pipe
 
-
-
 target = pipe.get_pipe()
+
 
 def sendDataToPipe(**kwargs):
 
     jsonDict = {
-        "key" : "sensor_data",
-        "data" : {
+        "key": "sensor_data",
+        "data": {
         }
     }
     for name, data in kwargs.items():
@@ -41,20 +40,22 @@ def sendDataToPipe(**kwargs):
         pipe.close_pipe(target)
         sys.exit()
 
+
 # Check OpenCV version
 assert cv.__version__ >= "4.9.0", \
-       "Please install latest opencv-python to try this demo: python3 -m pip install --upgrade opencv-python"
+    "Please install latest opencv-python to try this demo: python3 -m pip install --upgrade opencv-python"
 
 # Valid combinations of backends and targets
 backend_target_pairs = [
     [cv.dnn.DNN_BACKEND_OPENCV, cv.dnn.DNN_TARGET_CPU],
-    [cv.dnn.DNN_BACKEND_CUDA,   cv.dnn.DNN_TARGET_CUDA],
-    [cv.dnn.DNN_BACKEND_CUDA,   cv.dnn.DNN_TARGET_CUDA_FP16],
-    [cv.dnn.DNN_BACKEND_TIMVX,  cv.dnn.DNN_TARGET_NPU],
-    [cv.dnn.DNN_BACKEND_CANN,   cv.dnn.DNN_TARGET_NPU]
+    [cv.dnn.DNN_BACKEND_CUDA, cv.dnn.DNN_TARGET_CUDA],
+    [cv.dnn.DNN_BACKEND_CUDA, cv.dnn.DNN_TARGET_CUDA_FP16],
+    [cv.dnn.DNN_BACKEND_TIMVX, cv.dnn.DNN_TARGET_NPU],
+    [cv.dnn.DNN_BACKEND_CANN, cv.dnn.DNN_TARGET_NPU]
 ]
 
-parser = argparse.ArgumentParser(description='YuNet: A Fast and Accurate CNN-based Face Detector (https://github.com/ShiqiYu/libfacedetection).')
+parser = argparse.ArgumentParser(
+    description='YuNet: A Fast and Accurate CNN-based Face Detector (https://github.com/ShiqiYu/libfacedetection).')
 parser.add_argument('--input', '-i', type=str,
                     help='Usage: Set input to a certain image, omit if using camera.')
 parser.add_argument('--model', '-m', type=str, default='face_detection_yunet_2023mar.onnx',
@@ -80,15 +81,14 @@ parser.add_argument('--vis', '-v', action='store_true',
 args = parser.parse_args()
 
 
-
 def visualize(image, results, box_color=(0, 255, 0), text_color=(0, 0, 255), fps=None):
     output = image.copy()
     landmark_color = [
-        (255,   0,   0), # right eye
-        (  0,   0, 255), # left eye
-        (  0, 255,   0), # nose tip
-        (255,   0, 255), # right mouth corner
-        (  0, 255, 255)  # left mouth corner
+        (255, 0, 0),  # right eye
+        (0, 0, 255),  # left eye
+        (0, 255, 0),  # nose tip
+        (255, 0, 255),  # right mouth corner
+        (0, 255, 255)  # left mouth corner
     ]
 
     if fps is not None:
@@ -96,16 +96,17 @@ def visualize(image, results, box_color=(0, 255, 0), text_color=(0, 0, 255), fps
 
     for det in results:
         bbox = det[0:4].astype(np.int32)
-        cv.rectangle(output, (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3]), box_color, 2)
+        cv.rectangle(output, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), box_color, 2)
 
         conf = det[-1]
-        cv.putText(output, '{:.4f}'.format(conf), (bbox[0], bbox[1]+12), cv.FONT_HERSHEY_DUPLEX, 0.5, text_color)
+        cv.putText(output, '{:.4f}'.format(conf), (bbox[0], bbox[1] + 12), cv.FONT_HERSHEY_DUPLEX, 0.5, text_color)
 
-        landmarks = det[4:14].astype(np.int32).reshape((5,2))
+        landmarks = det[4:14].astype(np.int32).reshape((5, 2))
         for idx, landmark in enumerate(landmarks):
             cv.circle(output, landmark, 2, landmark_color[idx], 2)
 
     return output
+
 
 if __name__ == '__main__':
     backend_id = backend_target_pairs[args.backend_target][0]
@@ -132,8 +133,9 @@ if __name__ == '__main__':
         # Print results
         print('{} faces detected.'.format(results.shape[0]))
         for idx, det in enumerate(results):
-            print('{}: {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f}'.format(
-                idx, *det[:-1])
+            print(
+                '{}: {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f}'.format(
+                    idx, *det[:-1])
             )
 
         # Draw results on the input image
@@ -149,7 +151,7 @@ if __name__ == '__main__':
             cv.namedWindow(args.input, cv.WINDOW_AUTOSIZE)
             cv.imshow(args.input, image)
             cv.waitKey(0)
-    else: # Omit input to call default camera
+    else:  # Omit input to call default camera
         lookedAtScreenSeconds = []
         highestFaceCount = 0
         deviceId = 0
@@ -167,7 +169,7 @@ if __name__ == '__main__':
 
             # Inference
             tm.start()
-            results = model.infer(frame) # results is a tuple
+            results = model.infer(frame)  # results is a tuple
             tm.stop()
 
             print('{} faces detected.'.format(results.shape[0]))
@@ -182,14 +184,15 @@ if __name__ == '__main__':
                 lookedAtScreenSeconds.append(timeSecond)
                 started = False
                 jsonDict = {
-                    "key" : "sensor_data",
-                    "data" : {
-                        "HighestNumberOfFacesDetected" : highestFaceCount,
-                        "lookedAtScreenInSeconds" : lookedAtScreenSeconds
+                    "key": "sensor_data",
+                    "data": {
+                        "HighestNumberOfFacesDetected": highestFaceCount,
+                        "lookedAtScreenInSeconds": lookedAtScreenSeconds
                     }
                 }
                 jsonObj = json.dumps(jsonDict)
-                sendDataToPipe(HighestNumberOfFacesDetected=highestFaceCount, lookedAtScreenInSeconds=lookedAtScreenSeconds)
+                sendDataToPipe(HighestNumberOfFacesDetected=highestFaceCount,
+                               lookedAtScreenInSeconds=lookedAtScreenSeconds)
 
             # Draw results on the input image
             frame = visualize(frame, results, fps=tm.getFPS())
@@ -202,10 +205,10 @@ if __name__ == '__main__':
         print(lookedAtScreenSeconds)
         lookedAtScreenSeconds = sum(lookedAtScreenSeconds)
         jsonDict = {
-            "key" : "sensor_data",
-            "data" : {
-                "HighestNumberOfFacesDetected" : highestFaceCount,
-                "lookedAtScreenSeconds" : lookedAtScreenSeconds
+            "key": "sensor_data",
+            "data": {
+                "HighestNumberOfFacesDetected": highestFaceCount,
+                "lookedAtScreenSeconds": lookedAtScreenSeconds
             }
         }
         jsonObj = json.dumps(jsonDict)
--- client/run.js
+++ client/run.js
@@ -3,13 +3,7 @@ const {FrameJustWorks} = require("./lib/server");
 
 const clientServer = new FrameJustWorks()
 
-
 clientServer.start()
-
-
-
-
-
 
 clientServer.registerSensor('SensorOne', 'temperature')
 clientServer.registerSensor('SensorTwo', 'light')
