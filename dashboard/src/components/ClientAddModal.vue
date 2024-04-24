9975fb6593cdc3b6298bfbe60aa6e3d697c68dad
Daniel <daniel@canvy.com>
2024-04-24 13:48:13 +0200

    Auto stash before merge of "main" and "origin/main"

--- dashboard/src/components/ClientAddModal.vue
+++ dashboard/src/components/ClientAddModal.vue
@@ -5,31 +5,14 @@ import {VaModal} from "vuestic-ui";
 
 
 
-const addModal = ref<HTMLDialogElement | null>(null)
 
-const openModal= () => {
-  if (addModal) addModal.value?.showModal()
-}
+
 
 
 </script>
 
 <template>
-  <div>
-    <va-icon name="person_add" class="add_client_icon" color="success" @click="openModal"></va-icon>
-    <dialog
-        ref="addModal"
-        class="dialog"
-    >
-      <h3 class="va-h3">
-        Title
-      </h3>
-      <p>
-        Classic modal overlay which represents a dialog box or other interactive
-        component, such as a dismissible alert, sub-window, etc.
-      </p>
-    </dialog>
-  </div>
+
 
 
 
--- dashboard/src/components/clientsCard.vue
+++ dashboard/src/components/clientsCard.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import {onBeforeMount, ref} from 'vue'
 import axios from 'axios'
 
@@ -7,46 +7,47 @@ const loading = ref(false)
 const users = ref([])
 const selectedRows = ref(new Set<number>());
 const getUsers = async () => {
-  loading.value = true
-  const response = await axios.get('http://localhost:3001/get-all-users')
-  users.value = response.data
-  console.log(users.value)
-  setTimeout(() => {
-    loading.value = false
-  }, 1000)
+    loading.value = true
+    const response = await axios.get('http://localhost:3001/get-all-users')
+    users.value = response.data
+    console.log(users.value)
+    setTimeout(() => {
+        loading.value = false
+    }, 1000)
 }
 
 const deleteUser = async (index: number) => {
-  const id = users.value[index].CLIENT_ID
-  console.log(id)
-  if (!id) {
-    console.error("No CLIENT_ID provided")
-    return
-  }
-
-  const response = await axios.get('http://localhost:3001/delete-specific-user', {
-    params: {
-      appId: id
+    const id = users.value[index].CLIENT_ID
+    console.log(id)
+    if (!id) {
+        console.error("No CLIENT_ID provided")
+        return
     }
-  })
-  users.value.splice(index, 1)
+
+    const response = await axios.get('http://localhost:3001/delete-specific-user', {
+        params: {
+            appId: id
+        }
+    })
+    users.value.splice(index, 1)
 }
 
 const deleteSelectedUsers = async () => {
-  const sortedIndices = [...selectedRows.value].sort((a, b) => b - a);
-  for (const index of sortedIndices) {
-    await deleteUser(index);
-  }
+    const sortedIndices = [...selectedRows.value].sort((a, b) => b - a);
+    for (const index of sortedIndices) {
+        await deleteUser(index);
+    }
+    selectedRows.value = new Set();
 };
 
 const toggleSelection = (index: number) => {
-  const newSelection = new Set(selectedRows.value);
-  if (newSelection.has(index)) {
-    newSelection.delete(index);
-  } else {
-    newSelection.add(index);
-  }
-  selectedRows.value = newSelection;
+    const newSelection = new Set(selectedRows.value);
+    if (newSelection.has(index)) {
+        newSelection.delete(index);
+    } else {
+        newSelection.add(index);
+    }
+    selectedRows.value = newSelection;
 };
 
 const registerClient = async () => {
@@ -60,80 +61,72 @@ const registerClient = async () => {
     const registerResponse = await axios.post('http://localhost:3001/register-client', {CLIENT_ID: newClientId});
     console.log("Client registered:", registerResponse.data);
 
-  } catch (error) {
-    console.error("Error in client registration process", error);
-  } finally {
-    loading.value = false;
-  }
-};
-
-
 onBeforeMount(() => {
-  getUsers()
+    getUsers()
 })
 const columns = [
-  {key: "select"},
-  {key: "id", sortable: true},
-  {key: "CLIENT_ID", sortable: true},
-  {key: "actions", width: 80},
+    {key: "select"},
+    {key: "ID", sortable: true},
+    {key: "CLIENT_ID", sortable: true},
 ];
 
 const input = "";
 
+
+const defaultItem = {
+    ID: "",
+    CLIENT_ID: "",
+}
+
 </script>
 
 <template>
+    <va-card>
+        <va-card-title>
+            Clients
+        </va-card-title>
 
-  <va-card>
+        <va-card-content>
+            <VaDataTable v-if="users.length > 0"
+                         :columns="columns"
+                         :items="users"
+                         class="table-crud"
+                         striped>
 
-
-    <va-card-title>
-      Clients
-    </va-card-title>
-
-    <va-card-content>
-      <VaDataTable v-if="users.length > 0"
-                   class="table-crud"
-                   striped
-                   :items="users"
-                   :columns="columns"
-      >
-        <template #cell(select)="{ rowIndex }">
-          <va-checkbox :checked="selectedRows.has(rowIndex)" @click="() => toggleSelection(rowIndex)"/>
-        </template>
-
-        <template #cell(actions)="{ rowIndex }">
-          <VaButton
-              preset="plain"
-              icon="edit"
-              disabled
+                <template #cell(select)="{ rowIndex }">
+                    <input :checked="selectedRows.has(rowIndex)"
+                           type="checkbox"
+                           @click.stop="toggleSelection(rowIndex)"/>
+                </template>
 
 
-          />
-          <VaButton
-              preset="plain"
-              icon="delete"
-              class="ml-3"
-              @click="deleteUser(rowIndex)"
+            </VaDataTable>
+        </va-card-content>
 
-          />
-        </template>
-      </VaDataTable>
-    </va-card-content>
+        <va-card-actions align="right">
+            <va-button
+                v-if="selectedRows.size > 0"
+                color="danger"
+                icon="delete_sweep"
+                @click="deleteSelectedUsers">
+            </va-button>
+            <va-button :loading="loading" icon="refresh" @click="getUsers()"/>
+        </va-card-actions>
 
-    <va-card-actions align="right">
-      <va-button v-if="selectedRows.size > 0" @click="deleteSelectedUsers">Delete Selected</va-button>
-      <va-button icon="refresh" :loading="loading" @click="getUsers()"/>
-      <va-button color="primary" @click="registerClient">Generate New Client ID</va-button> <!-- New button here -->
-    </va-card-actions>
+    </va-card>
 
-  </va-card>
+
+    <va-modal ref="modal" stateful>
+        hello
+    </va-modal>
+
 </template>
 
 <style scoped>
-.tableActions {
-  display: flex;
-  justify-content: flex-end;
+
+
+input[type="checkbox"] {
+    transform: scale(1.4);
 }
 
 
