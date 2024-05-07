dc302f63b5db3ac177e48e1945370a26960e64e6
Daniel E. Sejersen <Daniel@canvy.com>
2024-05-07 13:43:52 +0200

    merge for some reason deleted client card fixed

--- backend/lib/EventMap.js
+++ backend/lib/EventMap.js
@@ -2,6 +2,18 @@ const eventMap = {
     'TemperatureExceedsMax': () => {
         console.log('executing TemperatureExceedsMax');
         return 0
+    },
+    'PeterErEnBozo': () => {
+        console.log('executing PeterErEnBozo');
+        return 0
+    },
+    'PatrickErEnBozo': () => {
+        console.log('executing PatrickErEnBozo');
+        return 0
+    },
+    'GustasErEnBozo': () => {
+        console.log('executing GustasErEnBozo');
+        return 0
     }
 }
 
