c40a8a47a6283abacabae888d28ab73b70ec92db
Daniel E. Sejersen <Daniel@canvy.com>
2024-05-07 12:49:35 +0200

    update

--- backend/lib/EventMap.js
+++ backend/lib/EventMap.js
@@ -1,5 +1,23 @@
 const eventMap = {
     'TemperatureExceedsMax': () => {
         console.log('executing TemperatureExceedsMax');
+        return 0
     }
 }
+
+
+console.log(eventMap['TemperatureExceedsMax']()); // prints 0
+/*
+foreach event in subbedEvents{
+    //find subscribed event
+
+    if(eventMap[event]){
+        //run event
+
+        eventMap[event]();
+    } else {
+        console.log('Event not found');
+    }
+
+}
+ */
